42e6204fd214b08797a69682f7286269c5747c2f
supreet mohapatra <supreetmohapatra06@gmail.com>
2026-07-07 11:56:55 +0530

    Hardcoded values

---
+++ src/constants.js
@@ -0,0 +1,28 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+export const WARDS_LIST = [
+  "Ward 1 - Sahadevkhunta",
+  "Ward 2 - Mallikashpur",
+  "Ward 3 - Kuruda",
+  "Ward 4 - Station Road",
+  "Ward 5 - Azimabad",
+  "Ward 6 - Nua Bazar",
+  "Ward 7 - Somanathpur",
+  "Ward 8 - Phuladi",
+  "Ward 9 - Angargadia",
+  "Ward 10 - Sunhat",
+  "Ward 11 - Arad Bazar",
+  "Ward 12 - Gopalgaon"
+];
+export const COMPLAINT_CATEGORIES = [
+  "Roads",
+  "Drainage & Sewage",
+  "Water Supply",
+  "Electricity & Streetlights",
+  "Education",
+  "Healthcare",
+  "Public Safety & Policing",
+  "Sanitation & Garbage"
+];
